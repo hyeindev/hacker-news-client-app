b703247e0b95f3c2fa38a7d287f6239609ae05ae
hyeindev <whgpdls1996@gmail.com>
2025-07-27 22:44:50 +0900

    feat: hackernews 피드 불러오기, document 이용하여 화면 출력

---
+++ app.js
@@ -0,0 +1,23 @@
+const ajax = new XMLHttpRequest();
+// const 에는 다른 값을 이후에 변경시킬 수 없음 (상수)
+
+const newsUrl = 'https://api.hnpwa.com/v0/news/1.json';
+
+// 데이터 불러오기
+ajax.open('GET',newsUrl,false); // false= 동기 처리 옵션
+ajax.send();
+
+const newsFeed = JSON.parse(ajax.response);
+const ul = document.createElement('ul');
+
+for(let i =0; i<10; i++) {
+  const li = document.createElement('li');
+  li.innerHTML = newsFeed[i].title;
+  ul.appendChild(li);
+}
+
+document.getElementById('root').appendChild(ul);
+
+
+
+
