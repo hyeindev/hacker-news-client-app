133a4a9d2f921aaf6ee838dbbc4a961f4d5df564
hyeindev <whgpdls1996@gmail.com>
2025-07-27 23:04:13 +0900

    feat: link 표현 추가, comment data 추가

--- dist/typescript-react-code-lab.f72d0d54.js
+++ dist/typescript-react-code-lab.f72d0d54.js
@@ -677,7 +677,10 @@ const newsFeed = JSON.parse(ajax.response);
 const ul = document.createElement('ul');
 for(let i = 0; i < 10; i++){
     const li = document.createElement('li');
-    li.innerHTML = newsFeed[i].title;
+    const a = document.createElement('a');
+    a.href = '#';
+    a.innerHTML = `${newsFeed[i].title} (${newsFeed[i].comments_count})`;
+    li.appendChild(a);
     ul.appendChild(li);
 }
 document.getElementById('root').appendChild(ul);
