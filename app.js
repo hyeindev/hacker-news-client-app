9d2a79d70a84bcbc7dd1e821125dbd504269f3a0
hyeindev <whgpdls1996@gmail.com>
2025-07-27 23:36:15 +0900

    refactor: ajax 데이터 가져오는 코드 함수로 리팩토링

--- app.js
+++ app.js
@@ -1,27 +1,25 @@
 const container = document.getElementById('root');
-
-const ajax = new XMLHttpRequest();
-// const 에는 다른 값을 이후에 변경시킬 수 없음 (상수)
-
+const ajax = new XMLHttpRequest(); // const 에는 다른 값을 이후에 변경시킬 수 없음 (상수)
 const newsUrl = 'https://api.hnpwa.com/v0/news/1.json';
 const contentUrl = "https://api.hnpwa.com/v0/item/@id.json";
-
 const content = document.createElement('div');
-// 데이터 불러오기
-ajax.open('GET',newsUrl,false); // false= 동기 처리 옵션
-ajax.send();
 
-const newsFeed = JSON.parse(ajax.response);
+function getData(url) {
+  // 데이터 불러오기
+  ajax.open('GET',url,false); // false= 동기 처리 옵션
+  ajax.send();
+
+  return JSON.parse(ajax.response);
+}
+
+const newsFeed = getData(newsUrl);
 const ul = document.createElement('ul');
 
 // hashchange 이벤트
 window.addEventListener('hashchange',function() {
   const id = this.location.hash.substring(1); // # 제거
-  // 이베늩 발생 시 내용 불러오기
-  ajax.open('GET',contentUrl.replace('@id',id),false);
-  ajax.send();
 
-  const newsContent = JSON.parse(ajax.response);
+  const newsContent = getData(contentUrl);
   const title = document.createElement('h1');
 
   title.innerHTML = newsContent.title;
@@ -29,8 +27,7 @@ window.addEventListener('hashchange',function() {
 });
 
 for(let i =0; i<10; i++) {
-const div = document.createElement('div');
-
+  const div = document.createElement('div');
   const li = document.createElement('li');
   const a = document.createElement('a');
 
